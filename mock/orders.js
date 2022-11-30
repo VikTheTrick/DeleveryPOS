0e5c9e9d78593eaffd3269b9af2f085c794b8b8a
fl1k <vgolubovic97@gmail.com>
2022-11-30 15:26:59 +0100

    More mock data

--- mock/orders.js
+++ mock/orders.js
@@ -1,77 +1,382 @@
 const orders = [
     {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
-        price: 1900,
+        price: 1600,
         delevery: true,
-        status: 0
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     },
     {
         companyLogo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdiscovery-cdn.wolt.com%2Fcities%2Fberlin%2Fsections%2F88ec475e-c5a6-11ea-a7b9-4a5b4d06897b_wolt_logo_article.jpeg&f=1&nofb=1&ipt=a5f0d1866e7f06d5a2a10f7621cb86cb2fe3eeeb57cc0ca76f59675ea2c6cd78&ipo=images",
         price: 1900,
         delevery: true,
-        status: 0
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
 
     }, {
         companyLogo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdiscovery-cdn.wolt.com%2Fcities%2Fberlin%2Fsections%2F88ec475e-c5a6-11ea-a7b9-4a5b4d06897b_wolt_logo_article.jpeg&f=1&nofb=1&ipt=a5f0d1866e7f06d5a2a10f7621cb86cb2fe3eeeb57cc0ca76f59675ea2c6cd78&ipo=images",
         price: 1900,
         delevery: true,
-        status: 0
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
 
     }, {
         companyLogo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdiscovery-cdn.wolt.com%2Fcities%2Fberlin%2Fsections%2F88ec475e-c5a6-11ea-a7b9-4a5b4d06897b_wolt_logo_article.jpeg&f=1&nofb=1&ipt=a5f0d1866e7f06d5a2a10f7621cb86cb2fe3eeeb57cc0ca76f59675ea2c6cd78&ipo=images",
         price: 1900,
         delevery: true,
-        status: 1
-
+        status: 1,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 0
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
 
     },
     , {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 1
+        status: 1,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
 
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 2
-
+        status: 2,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 0
-
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 0
-
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 0
-
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 0
-
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }, {
         companyLogo: "https://i.pinimg.com/originals/a8/71/4c/a8714cadb2358c6a32ce232025eb13fe.png",
         price: 1900,
         delevery: true,
-        status: 0
-
+        status: 0,
+        items: [
+            {
+                name: "Cheeseburger",
+                price: 700,
+                groups: [
+                    {
+                        name: "Sauces",
+                        items: ["Ketchup", "Mayonnaise"]
+                    },
+                    {
+                        name: "Added cheeses",
+                        items: ["Emmentaler", "Cheddar"]
+                    }
+                ]
+            },
+            {
+                name: "Coca cola",
+                price: 300,
+                groups: []
+            },
+            {
+                name: "French Fries",
+                price: 600,
+                groups: []
+            }
+        ]
     }
 ]
 
